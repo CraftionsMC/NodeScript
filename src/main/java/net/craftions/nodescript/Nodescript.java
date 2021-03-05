10e61527981e73484d7545448dcc73a0c9491b50
MCTzOCK <dev@mctzock.de>
2021-03-05 15:01:34 +0100

    Set CommandNode.java as executor for the "node" command.

--- src/main/java/net/craftions/nodescript/Nodescript.java
+++ src/main/java/net/craftions/nodescript/Nodescript.java
@@ -1,5 +1,6 @@
 package net.craftions.nodescript;
 
+import net.craftions.nodescript.commands.CommandNode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Nodescript extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class Nodescript extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getCommand("node").setExecutor(new CommandNode());
     }
 
     @Override
